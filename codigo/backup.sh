2994a217488cd22960f475bfd6384049fbac1fec
Henrique S. Xavier <contato@henriquexavier.net>
2025-10-22 10:27:24 -0300

    Backup shell script for locaweb VPS

---
+++ codigo/backup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# To run on locaweb VPS.
+
+# Hard-coded:
+bkp_prefix=usecases_test_
+bkp_suffix=.json
+src=/home/admin/ceweb/projetos/cordata/codigo/data/usecases_temp.json
+target=/home/admin/ceweb/projetos/cordata/dados/backups
+
+# Get current date:
+current_date=`date +'%Y-%m-%d'`
+
+# Make the copy (backup):
+echo "cp $src $target/$bkp_prefix$current_date$bkp_suffix"
+cp $src $target/$bkp_prefix$current_date$bkp_suffix
